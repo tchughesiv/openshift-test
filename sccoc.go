30dfd49842222c865406e0d7e77a36f753dc4851
tchughesiv <tchughesiv@gmail.com>
2017-10-01 19:36:15 -0500

    updates

--- sccoc.go
+++ sccoc.go
@@ -9,51 +9,48 @@ import (
 	securityapi "github.com/openshift/origin/pkg/security/apis/security"
 	"github.com/openshift/origin/pkg/security/securitycontextconstraints"
 	"github.com/openshift/origin/test/util"
-	kapi "k8s.io/kubernetes/pkg/api"
+//	kapi "k8s.io/kubernetes/pkg/api"
 )
 
 // const defaultScc = "anyuid"
 const defaultScc = "restricted"
 
 func main() {
-	var scc securityapi.SecurityContextConstraints
+	var scc *securityapi.SecurityContextConstraints
 	ns := testing.CreateNamespaceForTest()
-	ns.ObjectMeta.Name = util.RandomNamespace("tmp")
-	fmt.Printf("\n%#v\n\n", ns.Name)
-	fmt.Printf("%#v\n\n", ns.ObjectMeta.Annotations)
-
-	//groups, users := bp.GetBoostrapSCCAccess(bp.DefaultOpenShiftInfraNamespace)
+	ns.Name = util.RandomNamespace("tmp")
 	groups, users := bp.GetBoostrapSCCAccess(ns.Name)
 	bootstrappedConstraints := bp.GetBootstrapSecurityContextConstraints(groups, users)
-
 	for _, v := range bootstrappedConstraints {
 		if v.Name == defaultScc {
 			scc = v
+			scc.RunAsUser.UIDRangeMin = &(&struct{ x int64 }{1000100000}).x
+			scc.RunAsUser.UIDRangeMax = &(&struct{ x int64 }{1000110000}).x
 		}
 	}
-	
-	sccn := validNewSecurityContextConstraints(scc)
-	sccn.RunAsUser.UIDRangeMin = &(&struct{ x int64 }{1000100000}).x
-	sccn.RunAsUser.UIDRangeMax = &(&struct{ x int64 }{1000110000}).x
-	sccn.SELinuxContext.SELinuxOptions = &kapi.SELinuxOptions{
-		Level: "s0:c10,c5",
+
+/*
+	scc.SELinuxContext.SELinuxOptions = &kapi.SELinuxOptions{
+		Level: "s9:z0,z1",
 	}
+*/
 
-	fmt.Printf("%#v\n\n", sccn.Name)
-	fmt.Printf("%#v\n\n", sccn)
+    fmt.Printf("%#v\n\n", scc.Name)
+	fmt.Printf("%#v\n\n", scc)
 
-	_, err := securitycontextconstraints.NewSimpleProvider(sccn)
+	_, err := securitycontextconstraints.NewSimpleProvider(scc)
 	checkErr(err)
 }
+
 
 // convert specified scc definition into container runtime configs - using origin code
 // run image accordingly directly against container runtime... no ocp/k8s involvement
 
-type intwrapper struct{ x int64 }
-
-func validNewSecurityContextConstraints(scc securityapi.SecurityContextConstraints) *securityapi.SecurityContextConstraints {
-	return &scc
-}
+/*
+	func validNewSecurityContextConstraints(scc securityapi.SecurityContextConstraints) *securityapi.SecurityContextConstraints {
+		return &scc
+	}
+*/
 
 func checkErr(err error) {
 	if err != nil {
