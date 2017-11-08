e4097c73cdde69a51c9441051f82fb984d533f5d
tchughesiv <tchughesiv@gmail.com>
2017-11-08 12:06:03 -0600

    updates

--- sccoc.go
+++ sccoc.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"testing"
+	"time"
 
 	bp "github.com/openshift/origin/pkg/cmd/server/bootstrappolicy"
 	"github.com/openshift/origin/pkg/cmd/server/kubernetes/node"
@@ -15,7 +17,9 @@ import (
 	"github.com/openshift/origin/pkg/security/scc"
 	testutil "github.com/openshift/origin/test/util"
 	testserver "github.com/openshift/origin/test/util/server"
+	"k8s.io/kubernetes/pkg/apis/componentconfig"
 	"k8s.io/kubernetes/pkg/kubelet"
+	"k8s.io/kubernetes/pkg/api/v1"
 )
 
 func checkErr(err error) {
@@ -85,12 +89,12 @@ func main() {
 	err = os.RemoveAll(etcdt.DataDir)
 	checkErr(err)
 
-	// reference Admit function vendor/github.com/openshift/origin/pkg/security/admission/admission.go
 	fmt.Printf("\n")
 	provider, ns, err := scc.CreateProviderFromConstraint(ns.Name, ns, sccn, clusterAdminKubeClientset)
 	checkErr(err)
-	// testis := testgen.MockImageStream("centos", "docker.io/centos", map[string]string{"latest": "latest"})
-	// testpod := testutil.CreatePodFromImage(testis, "latest", ns.Name)
+
+
+	// !! can go straight k8s from here on out... 
 	testpod := network.GetTestPod(defaultImage, "tcp", "tmp", "localhost", 12000)
 
 	tc := &testpod.Spec.Containers[0]
@@ -98,40 +102,54 @@ func main() {
 	checkErr(err)
 
 	fmt.Printf("\n%#v\n\n", tc.SecurityContext)
-	// fmt.Printf("%#v\n\n", tc.SecurityContext.Capabilities)
-	// fmt.Printf("%#v\n\n", tc.SecurityContext.SELinuxOptions.Level)
-	// fmt.Printf("%#v\n\n", dcfg.Endpoint)
 	fmt.Printf("Using %#v scc...\n\n", provider.GetSCCName())
-	// fmt.Printf("%#v\n\n", dcfg.Endpoint)
 
-	// vendoring issues w/ kubelet packages
+	// !!! vendoring issues w/ kubelet packages
 	// vendor/k8s.io/kubernetes/vendor/k8s.io/client-go/util/flowcontrol/throttle.go:59: undefined: ratelimit.Clock
-
-	// SOOO, instead use vendor/github.com/openshift/origin/pkg/cmd/server/start/start_node.go to build a NodeConfig
-	// which has everything you need below to GenerateRunContainerOptions
-	// nodeConfig, configFile, err := resolveNodeConfig()
-	// nconfig.MasterKubeConfig
-
-	// start.Run()
 
 	kserver := nodeconfig.KubeletServer
 	kubeDeps := nodeconfig.KubeletDeps
-	kubeCfg := kubelet.KubeletBootstrap.GetConfiguration()
-	k, err := kubelet.NewMainKubelet(kubeCfg, kubeDeps, true, kserver.DockershimRootDirectory)
-	//	k, err := kubelet.NewMainKubelet(nodeconfig.KubeletConfiguration, kubeDeps, true, nodeconfig.DockershimRootDirectory)
-	//	checkErr(err)
-	//	rco, _, err := k.GenerateRunContainerOptions(testpod, tc, nodeconfig.Address)
-	//	checkErr(err)
-	fmt.Printf("%#v\n\n", kubeCfg.Address)
-	//	fmt.Printf("%#v\n\n", rco)
+	kubeCfg := &componentconfig.KubeletConfiguration{}
+	kubeCfg.SyncFrequency.Duration = 10 * time.Second
+	// How the Kubelet should setup hairpin NAT. Can take the values: "promiscuous-bridge"
+	// (make cbr0 promiscuous), "hairpin-veth" (set the hairpin flag on veth interfaces)
+	// or "none" (do nothing).
+	kubeCfg.HairpinMode = "none"
+	kubeCfg.NetworkPluginName = "cni"
+	
+	if tempDir, err := ioutil.TempDir("/tmp", "kubelet_test."); err != nil {
+		t.Fatalf("can't make a temp rootdir: %v", err)
+	} else {
+		kubeCfg.RootDirectory = tempDir
+	}
+	if err := os.MkdirAll(kubeCfg.RootDirectory, 0750); err != nil {
+		t.Fatalf("can't mkdir(%q): %v", kubeCfg.RootDirectory, err)
+	}
 
-	//	node.BuildKubernetesNodeConfig()
+	fmt.Printf("%#v\n\n", kubeDeps.CAdvisorInterface)
+	fmt.Printf("%#v\n\n", kubeCfg.CAdvisorPort)
+	
+	k, err := kubelet.NewMainKubelet(kubeCfg, kubeDeps, true, kserver.DockershimRootDirectory)
+	checkErr(err)
+
+	v1Pod := &v1.Pod{}
+	err = v1.Convert_api_Pod_To_v1_Pod(testpod, v1Pod, nil)
+	checkErr(err)
+	tv1c := &v1Pod.Spec.Containers[0]
+	
+	fmt.Printf("%#v\n\n", kserver.ContainerRuntime)
+	// fmt.Printf("%#v\n\n", kubeCfg.RootDirectory)
+	// fmt.Printf("%#v\n\n", kubeCfg.ContainerRuntime)
+
+	rco, _, err := k.GenerateRunContainerOptions(v1Pod, tv1c, v1Pod.Status.PodIP)
+
+	fmt.Printf("%#v\n\n", rco)
 
 	// ?? reference for container runtime -
 	// vendor/github.com/openshift/origin/vendor/k8s.io/kubernetes/pkg/kubelet/kubelet.go
 	// vendor/github.com/openshift/origin/vendor/k8s.io/kubernetes/pkg/kubectl/run_test.go
 	// kubectl run reference: https://github.com/openshift/kubernetes/blob/openshift-1.6-20170501/pkg/kubectl/run_test.go
-
+	// dockertools.NewDockerManager()
 	// dockerRun(tc.Image, dockerVersion)
 }
 
