45777772508b946c91530e5cd6bbbf642a6d62c9
tchughesiv <tchughesiv@gmail.com>
2017-11-20 11:32:37 -0600

    updates

--- cmd/sccoc/sccoc.go
+++ cmd/sccoc/sccoc.go
@@ -11,9 +11,14 @@ import (
 	configapi "github.com/openshift/origin/pkg/cmd/server/api"
 	bp "github.com/openshift/origin/pkg/cmd/server/bootstrappolicy"
 	cmdutil "github.com/openshift/origin/pkg/cmd/util"
+	"github.com/openshift/origin/pkg/cmd/util/clientcmd"
 	"github.com/openshift/origin/pkg/cmd/util/serviceability"
 	"github.com/openshift/origin/pkg/oc/cli"
+	"github.com/openshift/origin/pkg/oc/cli/config"
+	testutil "github.com/openshift/origin/test/util"
 	testserver "github.com/openshift/origin/test/util/server"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kclientcmd "k8s.io/client-go/tools/clientcmd"
 	"k8s.io/kubernetes/pkg/util/logs"
 
 	// install all APIs
@@ -37,10 +42,15 @@ import (
 
 func main() {
 	var sccopts []string
-	clArgs := os.Args
 	sflag := cmdutil.Env("OPENSHIFT_SCC", bp.SecurityContextConstraintRestricted)
-	os.Setenv("TEST_ETCD_DIR", "/tmp/etcdtest")
+	os.Setenv("TEST_ETCD_DIR", testutil.GetBaseDir()+"/etcd")
 
+	if os.Args[1] != "run" {
+		fmt.Printf("\nError: unknown command %#v for %#v... must use \"run\"\n", os.Args[1], os.Args[0])
+		os.Exit(1)
+	}
+	//app := os.Args[2]
+	clArgs := os.Args
 	groups, users := bp.GetBoostrapSCCAccess(bp.DefaultOpenShiftInfraNamespace)
 	for _, v := range bp.GetBootstrapSecurityContextConstraints(groups, users) {
 		sccopts = append(sccopts, v.Name)
@@ -59,8 +69,7 @@ func main() {
 	// os.Setenv("KUBELET_NETWORK_ARGS", "")
 	mconfig, nconfig, _, err := testserver.DefaultAllInOneOptions()
 	checkErr(err)
-	//mpath := nconfig.VolumeDirectory + "/manifests"
-	mpath := "/tmp/manifests"
+	mpath := testutil.GetBaseDir() + "/manifests"
 	nconfig.PodManifestConfig = &configapi.PodManifestConfig{
 		Path: mpath,
 		FileCheckIntervalSeconds: int64(5),
@@ -78,9 +87,6 @@ func main() {
 		checkErr(err)
 		kubeDeps := nodeconfig.KubeletDeps
 	*/
-	//oaclient, err := testutil.GetClusterAdminClient(kconfig)
-	//checkErr(err)
-
 	logs.InitLogs()
 	defer logs.FlushLogs()
 	defer serviceability.BehaviorOnPanic(os.Getenv("OPENSHIFT_ON_PANIC"))()
@@ -121,13 +127,33 @@ func main() {
 		os.Exit(1)
 	}
 
+	cfg, err := config.NewOpenShiftClientConfigLoadingRules().Load()
+	checkErr(err)
+	defaultCfg := kclientcmd.NewDefaultClientConfig(*cfg, &kclientcmd.ConfigOverrides{})
+	f := clientcmd.NewFactory(defaultCfg)
+	clientset, err := f.ClientSet()
+	checkErr(err)
+	aoapp, err := f.OpenshiftInternalAppsClient()
+	checkErr(err)
+
+	//n, err := clientset.Core().Namespaces().Get("default", metav1.GetOptions{})
+	//checkErr(err)
+	dc, err := aoapp.Apps().DeploymentConfigs("default").List(metav1.ListOptions{})
+	checkErr(err)
+	fmt.Println(dc.Items[0].GetName())
+	pod, err := f.PodForResource(dc.Items[0].GetName(), time.Second*10)
+	checkErr(err)
+	fmt.Println(pod)
+	pint := clientset.Core().Pods("default")
+	plist, err := pint.List(metav1.ListOptions{})
+	checkErr(err)
+	fmt.Println(plist.Items)
+
 	/*
 		s.RunOnce = true
 		err = app.Run(s, kubeDeps)
 		checkErr(err)
 	*/
-
-	// fmt.Printf("%#v\n", kubeDeps.NetworkPlugins)
 
 	/*
 		fmt.Printf("\n")
@@ -153,49 +179,3 @@ func contains(sccopts []string, sflag string) bool {
 	}
 	return false
 }
-
-/*
-	// deploy registry
-	rmount := nconfig.VolumeDirectory + "/registry"
-	dc := oaclient.DeploymentConfigs(openshift.DefaultNamespace)
-	dcg, err := dc.Get("docker-registry", metav1.GetOptions{})
-	checkErr(err)
-	if dcg.GetName() != "" {
-		if dcg.Status.ReadyReplicas == 0 {
-			fmt.Printf("\n")
-			os.Args = []string{"oc", "delete", "dc/docker-registry", "svc/docker-registry"}
-			if err := command.Execute(); err != nil {
-				os.Exit(1)
-			}
-			os.Args = []string{"oc", "delete", "clusterrolebinding.authorization.openshift.io", "registry-registry-role"}
-			if err := command.Execute(); err != nil {
-				os.Exit(1)
-			}
-			os.Args = []string{"oc", "delete", "sa", "registry"}
-			if err := command.Execute(); err != nil {
-				os.Exit(1)
-			}
-			// ?? add a loop until dc cleared???
-		}
-	}
-	dcg, err = dc.Get("docker-registry", metav1.GetOptions{})
-	checkErr(err)
-	if dcg.GetName() == "" {
-		fmt.Printf("\n")
-		if _, err := os.Stat(rmount); os.IsNotExist(err) {
-			os.Mkdir(rmount, 0750)
-		}
-		os.Args = []string{"oc", "adm", "registry", "--service-account=registry", "--config=" + kconfig, "--mount-host=" + rmount}
-		// os.Args = []string{"oc", "adm", "registry"}
-		if err := command.Execute(); err != nil {
-			os.Exit(1)
-		}
-
-		// ensure registry comes up
-		fmt.Printf("\n")
-		os.Args = []string{"oc", "rollout", "status", "dc/docker-registry", "-w"}
-		if err := command.Execute(); err != nil {
-			os.Exit(1)
-		}
-	}
-*/
